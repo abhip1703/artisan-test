0340372b32358dd3b7d32e3321d7d1abeff74f03
Abhishek Paradkar <abhishek@Abhisheks-MacBook-Air.local>
2018-06-21 19:27:34 +0530

    config update

--- artisan/shopping/admin/include/config.php
+++ artisan/shopping/admin/include/config.php
@@ -10,7 +10,17 @@ $db['default']['hostname'] = $cleardb_server;
 $db['default']['username'] = $cleardb_url["user"];
 $db['default']['password'] = $cleardb_url["pass"];
 $db['default']['database'] = substr($cleardb_url["path"], 1);
-
+db['default']['dbdriver'] = 'mysql';
+$db['default']['dbprefix'] = '';
+$db['default']['pconnect'] = TRUE;
+$db['default']['db_debug'] = TRUE;
+$db['default']['cache_on'] = FALSE;
+$db['default']['cachedir'] = '';
+$db['default']['char_set'] = 'utf8';
+$db['default']['dbcollat'] = 'utf8_general_ci';
+$db['default']['swap_pre'] = '';
+$db['default']['autoinit'] = TRUE;
+$db['default']['stricton'] = FALSE;
 
 // define('DB_SERVER','localhost');
 // define('DB_USER','root');
